68e56f63494906c7428fd6876d940dae5e5ef99f
Aditya Kumar Singh <94990196+anonymouslyfadeditzme@users.noreply.github.com>
2023-01-18 19:16:41 +0530

    Did Onbording Scrolling part with data file

--- lib/screens/onboardingscreen.dart
+++ lib/screens/onboardingscreen.dart
@@ -1,3 +1,4 @@
+import 'package:bloodbank_app/constants/onboardingdata.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -9,27 +10,78 @@ class MyHomePage extends StatelessWidget {
       body: SafeArea(
         child: Container(
           color: Color(0xffFA4848),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                "assets/health_check.png",
-              ),
-              Container(
-                padding: EdgeInsets.only(
-                  left: 57,
-                  right: 56,
-                ),
-                child: Text(
-                  "To be a responsible donor, you must get a check-up.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: onboardingData
+                  .map(
+                    (onboardingSingleData) => Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset(
+                          onboardingSingleData["image_url"]!,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(
+                            left: 57,
+                            right: 56,
+                          ),
+                          child: Text(
+                            onboardingSingleData["text"]!,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                  .toList(),
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Container(
+//           color: Color(0xffFA4848),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: [
+//               Image.asset(
+//                 "assets/health_check.png",
+//               ),
+//               Container(
+//                 padding: EdgeInsets.only(
+//                   left: 57,
+//                   right: 56,
+//                 ),
+//                 child: Text(
+//                   "To be a responsible donor, you must get a check-up.",
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(fontSize: 18, color: Colors.white),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
