aab25ecb96436335ddc58a2f14c2720d7a4bf61b
Aditya Kumar Singh <94990196+anonymouslyfadeditzme@users.noreply.github.com>
2023-01-19 19:17:10 +0530

    Did Onboarding and splash together on own

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Blood Bank App',
-      home: const MyHomePage(),
+      home: const OnboardingPage(),
     );
   }
 }
--- lib/screens/onboardingscreen.dart
+++ lib/screens/onboardingscreen.dart
@@ -1,37 +1,24 @@
 import 'package:bloodbank_app/constants/onboardingdata.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          color: Color(0xffFA4848),
+          color: Colors.red,
+          width: double.infinity,
           child: SingleChildScrollView(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: onboardingData
                   .map(
-                    (onboardingSingleData) => Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.asset(
-                          onboardingSingleData["image_url"]!,
-                        ),
-                        Container(
-                          padding: EdgeInsets.only(
-                            left: 57,
-                            right: 56,
-                          ),
-                          child: Text(
-                            onboardingSingleData["text"]!,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          ),
-                        ),
-                      ],
+                    (singleOnboardingScreen) => onboardingWidget(
+                      imageUrl: singleOnboardingScreen["image_url"]!,
+                      text: singleOnboardingScreen["text"]!,
                     ),
                   )
                   .toList(),
@@ -41,7 +28,89 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
+
+  Widget onboardingWidget(
+      {required String imageUrl, String text = "", int? index}) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          child: Image.asset(
+            imageUrl,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(
+            left: 57.0,
+            right: 56.0,
+          ),
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
+
+
+
+// import 'package:bloodbank_app/constants/onboardingdata.dart';
+// import 'package:flutter/material.dart';
+
+// class OnboardingPage extends StatelessWidget {
+//   const OnboardingPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Container(
+//           color: Color(0xffFA4848),
+//           width: double.infinity,
+//           child: SingleChildScrollView(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: onboardingData
+//                   .map(
+//                     (onboardingSingleData) =>
+//                         onboardingWidget(onboardingSingleData),
+//                   )
+//                   .toList(),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Column onboardingWidget(Map<String, String> onboardingSingleData) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//       children: [
+//         Image.asset(
+//           onboardingSingleData["image_url"]!,
+//         ),
+//         Container(
+//           padding: EdgeInsets.only(
+//             left: 57,
+//             right: 56,
+//           ),
+//           child: Text(
+//             onboardingSingleData["text"]!,
+//             textAlign: TextAlign.center,
+//             style: TextStyle(fontSize: 18, color: Colors.white),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
 
 
 
